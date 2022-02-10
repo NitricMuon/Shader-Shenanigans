a5b3e9cb2463a8797fef80f4de6622756ea05a5e
NitricMuon <bradley.maher@gmail.com>
2022-02-09 22:49:34 -0500

    please help me

--- Assets/SlimeStuff/CGoL/CGoLManager.cs
+++ Assets/SlimeStuff/CGoL/CGoLManager.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Compression;
+using System.Net.Http.Headers;
 using UnityEngine;
+using UnityEngine.Accessibility;
 using UnityEngine.Experimental.Rendering;
 
 public class CGoLManager : MonoBehaviour
 {
-    public const GraphicsFormat defaultGFormat = GraphicsFormat.R16G16B16A16_SFloat;
+    private const GraphicsFormat defaultGFormat = GraphicsFormat.R16G16B16A16_SFloat;
+
+    [HeaderAttribute("Simulation Size")]
     public int width = 2560;
     public int height = 1440;
 
+    [HeaderAttribute("Simulation Settings")]
+    public int rangeStart = 2;
+    public int rangeEnd = 3;
+    public int spawn = 3;
+
     public ComputeShader compute;
     RenderTexture displayTexture;
+    RenderTexture processTexture;
+
+    ComputeBuffer randomData;
+
+    private int updateKernel = 0;
+    private int displayKernel = 1;
 
     void Init()
     {
@@ -19,7 +35,22 @@ public class CGoLManager : MonoBehaviour
         displayTexture.autoGenerateMips = false;
         displayTexture.Create();
 
-        compute.SetTexture(0, "TargetTexture", displayTexture);
+        processTexture = new RenderTexture(width, height, 0, defaultGFormat);
+        processTexture.enableRandomWrite = true;
+        processTexture.autoGenerateMips = false;
+        processTexture.Create();
+
+        compute.SetTexture(updateKernel, "displayTexture", displayTexture);
+        compute.SetTexture(updateKernel, "processTexture", processTexture);
+
+        compute.SetTexture(displayKernel, "displayTexture", displayTexture);
+        compute.SetTexture(displayKernel, "processTexture", processTexture);
+
+        compute.SetInt("width", width);
+        compute.SetInt("height", height);
+        compute.SetInt("rangeStart", rangeStart);
+        compute.SetInt("rangeEnd", rangeEnd);
+        compute.SetInt("spawn", spawn);
     }
 
     // Start is called before the first frame update
@@ -27,12 +58,42 @@ public class CGoLManager : MonoBehaviour
     {
         Init();
         GetComponent<MeshRenderer>().material.mainTexture = displayTexture;
+
+        int stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector2));
+        int count = width * height;
+        int initialCells = 1000000;
+        randomData = new ComputeBuffer(count, stride);
+
+        Vector2[] cells = new Vector2[initialCells];
+
+        for (int i = 0; i < initialCells; i++)
+        {
+            cells[i] = new Vector2Int(Random.Range(0, width), Random.Range(0, height));
+        }
+        randomData.SetData(cells);
+        compute.SetBuffer(2, "randomData", randomData);
+        compute.SetTexture(2, "displayTexture", displayTexture);
+        
+        int threadGroupX = Mathf.CeilToInt(count/64);
+        compute.Dispatch(2, threadGroupX, 1, 1);
+
+        randomData.Release();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        compute.SetInt("width", width);
+        compute.SetInt("height", height);
+        compute.SetInt("rangeStart", rangeStart);
+        compute.SetInt("rangeEnd", rangeEnd);
+        compute.SetInt("spawn", spawn);
+
+        int threadGroupX = Mathf.CeilToInt(width/8);
+        int threadGroupY = Mathf.CeilToInt(height/8);
+        //compute.Dispatch(updateKernel, threadGroupX, threadGroupY, 1);
+
+        compute.Dispatch(displayKernel, threadGroupX, threadGroupY, 1);
     }
 
     void FixedUpdate()
@@ -42,6 +103,6 @@ public class CGoLManager : MonoBehaviour
 
     void LateUpdate()
     {
-
+        
     }
 }
