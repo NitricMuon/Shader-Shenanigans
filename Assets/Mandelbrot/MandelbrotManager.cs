524da5cd1300c0bf5c2cff7a7ae256c9399fe3c8
NitricMuon <bradley.maher@gmail.com>
2022-02-28 21:14:25 -0500

    mmm fractal

--- Assets/Mandelbrot/MandelbrotManager.cs
+++ Assets/Mandelbrot/MandelbrotManager.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Numerics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MandelbrotManager : MonoBehaviour
 {
@@ -10,10 +9,10 @@ public class MandelbrotManager : MonoBehaviour
 
     [HeaderAttribute("Simulation Settings")]
     public ComputeShader compute;
-    public int maxInterations = 100;
-    public int scalar = 1;
-    public UnityEngine.Vector2 minimums;
-    public UnityEngine.Vector2 maximums;
+    public float maxIterations = 256f;
+    public float scalar = 1f; // for zooming in and stuff
+    public Vector2 botleft = new Vector2(-2.00f, -1.12f);
+    public Vector2 topright = new Vector2(0.47f, 1.12f);
 
     private int threadGroupX;
     private int threadGroupY;
@@ -28,18 +27,33 @@ public class MandelbrotManager : MonoBehaviour
 
         Helper.CreateRenderTexture(ref result, width, height);
 
-        compute.SetVector("minimums", minimums);
-        compute.SetVector("maximums", maximums);
+        compute.SetFloat("scalar", scalar);
+        compute.SetFloat("maxIterations", maxIterations);
+        compute.SetVector("botleft", botleft);
+        compute.SetVector("topright", topright);
+        compute.SetVector("screenSize", new Vector2(width, height));
+        compute.SetTexture(0, "result", result);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
+        GetComponent<MeshRenderer>().material.mainTexture = result;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        compute.Dispatch(0, threadGroupX, threadGroupY, 1);
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 dir = value.Get<Vector2>();
+    }
+
+    void OnZoom(InputValue value)
     {
         
     }
